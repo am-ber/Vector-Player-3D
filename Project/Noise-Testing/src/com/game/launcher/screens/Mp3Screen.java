ca1677568f55ba96bac58d550cad2d1136aad684
zach-v <zachary.vanscoit@siu.edu>
2017-11-08 14:07:09 -0600

    Removed things not needed

--- Project/Noise-Testing/src/com/game/launcher/screens/Mp3Screen.java
+++ Project/Noise-Testing/src/com/game/launcher/screens/Mp3Screen.java
@@ -5,7 +5,7 @@ import java.awt.Color;
 import java.awt.Graphics2D;
 import java.io.File;
 import java.io.IOException;
-import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioInputStream;
@@ -15,6 +15,17 @@ import javax.sound.sampled.UnsupportedAudioFileException;
 import com.game.engine.Game;
 
 import javazoom.spi.mpeg.sampled.convert.DecodedMpegAudioInputStream;
+import net.bramp.ffmpeg.FFmpeg;
+import net.bramp.ffmpeg.FFmpegExecutor;
+import net.bramp.ffmpeg.FFmpegUtils;
+import net.bramp.ffmpeg.FFprobe;
+import net.bramp.ffmpeg.builder.FFmpegBuilder;
+import net.bramp.ffmpeg.job.FFmpegJob;
+import net.bramp.ffmpeg.probe.FFmpegFormat;
+import net.bramp.ffmpeg.probe.FFmpegProbeResult;
+import net.bramp.ffmpeg.probe.FFmpegStream;
+import net.bramp.ffmpeg.progress.Progress;
+import net.bramp.ffmpeg.progress.ProgressListener;
 
 public class Mp3Screen extends Screen{
 	
@@ -52,6 +63,8 @@ public class Mp3Screen extends Screen{
 		}
 		
 		System.out.println("length of buffer: "+len);
+		
+		encode();
 	}
 	
 	// Obj
@@ -68,24 +81,24 @@ public class Mp3Screen extends Screen{
 	
 	@Override
 	public void update() {
-//		thread.run();
-		float[] equalizer;
-		try {
-		    // DecodedMpegAudioInputStream properties
-		    if (din instanceof javazoom.spi.PropertiesContainer) {
-		    }
-			Map properties = ((javazoom.spi.PropertiesContainer) din).properties(); 
-			
-			equalizer = (float[]) properties.get("mp3.equalizer");
-			equalizer[0] = 0.5f;
-			equalizer[31] = 0.25f;
-			
-//			din.read();
-			streemy.read();
-			
-		} catch(Exception e){
-			e.printStackTrace();
-		}
+////		thread.run();
+//		float[] equalizer;
+//		try {
+//		    // DecodedMpegAudioInputStream properties
+//		    if (din instanceof javazoom.spi.PropertiesContainer) {
+//		    }
+//			Map properties = ((javazoom.spi.PropertiesContainer) din).properties(); 
+//			
+//			equalizer = (float[]) properties.get("mp3.equalizer");
+//			equalizer[0] = 0.5f;
+//			equalizer[31] = 0.25f;
+//			
+////			din.read();
+//			streemy.read();
+//			
+//		} catch(Exception e){
+//			e.printStackTrace();
+//		}
 		
 		for(int i=0; i < columns-1; i++) {
 			noiseGrid[i] = noiseGrid[i+1];
@@ -96,6 +109,85 @@ public class Mp3Screen extends Screen{
 //			System.out.println("val is: "+vals.get(index));
 		
 		index ++;
+	}
+	
+	private void encode() {
+		
+		try {
+			FFmpeg ffmpeg = new FFmpeg("E:/Audio/Music/my music/Techno/Genre/DnB/");
+			FFprobe ffprobe = new FFprobe("E:/Audio/Music/my music/Techno/Genre/DnB/");
+	
+			FFmpegBuilder builder = new FFmpegBuilder()
+	
+			  .setInput("Krewella - Come & Get It.mp3")     // Filename, or a FFmpegProbeResult
+			  .overrideOutputFiles(true) // Override the output if it exists
+	
+			  .addOutput("output.mp4")   // Filename for the destination
+			    .setFormat("wav")        // Format is inferred from filename, or can be set
+	//		    .setTargetSize(250_000)  // Aim for a 250KB file
+	//
+	//		    .disableSubtitle()       // No subtiles
+	//
+	//		    .setAudioChannels(1)         // Mono audio
+	//		    .setAudioCodec("aac")        // using the aac codec
+	//		    .setAudioSampleRate(48_000)  // at 48KHz
+	//		    .setAudioBitRate(32768)      // at 32 kbit/s
+	//
+	//		    .setVideoCodec("libx264")     // Video using x264
+	//		    .setVideoFrameRate(24, 1)     // at 24 frames per second
+	//		    .setVideoResolution(640, 480) // at 640x480 resolution
+	//
+	//		    .setStrict(FFmpegBuilder.Strict.EXPERIMENTAL) // Allow FFmpeg to use experimental specs
+			    .done();
+	
+			FFmpegExecutor executor = new FFmpegExecutor(ffmpeg, ffprobe);
+	
+			// Run a one-pass encode
+			executor.createJob(builder).run();
+			
+			FFmpegProbeResult probeResult = ffprobe.probe("Krewella - Come & Get It.mp3");
+	
+			FFmpegFormat format = probeResult.getFormat();
+			System.out.format("%nFile: '%s' ; Format: '%s' ; Duration: %.3fs", 
+				format.filename, 
+				format.format_long_name,
+				format.duration
+			);
+	
+			FFmpegStream stream = probeResult.getStreams().get(0);
+			System.out.format("%nCodec: '%s' ; Width: %dpx ; Height: %dpx",
+				stream.codec_long_name,
+				stream.width,
+				stream.height
+			);
+	
+			FFmpegProbeResult in = ffprobe.probe("Krewella - Come & Get It.mp3");
+	
+			FFmpegJob job = executor.createJob(builder, new ProgressListener() {
+	
+				// Using the FFmpegProbeResult determine the duration of the input
+				final double duration_ns = in.getFormat().duration * TimeUnit.SECONDS.toNanos(1);
+	
+				@Override
+				public void progress(Progress progress) {
+					double percentage = progress.out_time_ns / duration_ns;
+	
+					// Print out interesting information about the progress
+					System.out.println(String.format(
+						"[%.0f%%] status:%s frame:%d time:%s ms fps:%.0f speed:%.2fx",
+						percentage * 100,
+						progress.status,
+						progress.frame,
+						FFmpegUtils.toTimecode(progress.out_time_ns, TimeUnit.NANOSECONDS),
+						progress.speed
+					));
+				}
+			});
+	
+			job.run();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public Game getGame(){
